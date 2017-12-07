4ae056814686db94fc82fbf500d23d344779ac2c
Klim <klimka13@ya.ru>
2017-12-07 10:40:10 +0200

    added Hello World message

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            List<int> listing = new List<int>();
+            listing.Add(8);
+            listing.Add(13);
+            listing.Add(21);
+            listing.Add(34);
+            listing.Add(709);
+
+            int k;
+            for (k = 0; k < listing.Count; k++)
+            Console.Write(listing[k] + " ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
